635a0d8d464dc53b8d63acfffdcac4ff898c226f
Alois Sečkár <ellrohir@seznam.cz>
2024-05-02 11:02:54 +0200

    feat: update the service according to JEP index changes

--- server/api/peek.ts
+++ server/api/peek.ts
@@ -12,31 +12,43 @@ export default defineEventHandler(async (): Promise<JEPData> => {
 
   const jepPage = parse(jepHTMLData)
 
-  // entries are listed in two <table class="jeps"> elements
+  // entries are listed in couple of <table class="jeps"> elements
   const tables = jepPage.querySelectorAll('.jeps')
-  // first table containts the actual JEPs
-  const jepTable = tables[0]
-  // second table lists drafts yet to be promoted to JEP (or left forsaken)
-  const draftTable = tables[1]
 
-  // now we seek last <tr> of each table - here will be the latest entry
-  const lastJEPRow = jepTable?.querySelectorAll('tr')?.at(-1)
-  const lastDraftRow = draftTable?.querySelectorAll('tr')?.at(-1)
+  // first table contains "Process JEPs" => skip
 
+  // second table contains "In-flight JEPs"
+  // - those already have proper JEP number, so the "latest JEP" would be here
+  const jepTable = tables[1]
+  // entries are ordered "newest first" - data will be in the first <tr> row
+  const latestJEPRow = jepTable?.querySelectorAll('tr')?.at(0)
   // the target number resides inside (the only one) <td class="jep">
   // element nested inside <tr>'s chlidren
-  const lastJEPCell = lastJEPRow?.querySelector('.jep')
-  const lastDraftCell = lastDraftRow?.querySelector('.jep')
-
+  const latestJEPCell = latestJEPRow?.querySelector('.jep')
   // finally the value is a sole TextNode inside <td> we just grabbed
   // and we can access it directly via .text attribute
-  const lastJEPNo = lastJEPCell?.text
-  const lastDraftNo = lastDraftCell?.text
+  const latestJEPNo = latestJEPCell?.text
+
+  // third table contains "Submitted JEPs"
+  // - they passed the first level and are likely to be worked on, so we use this one
+  const draftTable = tables[2]
+  // entries are not clearly ordered - we need to find correct row by going through all
+  let lastDraftNo = -1
+  const draftTableRows = draftTable?.querySelectorAll('tr')
+  draftTableRows?.forEach((row) => {
+    // get current value
+    const draftNoCell = row?.querySelector('.jep')
+    const draftNo = draftNoCell?.text
+    // if it is higher than previous max, set it
+    if (draftNo && parseInt(draftNo) > lastDraftNo) {
+      lastDraftNo = parseInt(draftNo)
+    }
+  })
 
   // return JSON data
   return {
     fetched: new Date(),
-    lastJEP: parseInt(lastJEPNo || '-1'),
-    lastDraft: parseInt(lastDraftNo || '-1'),
+    lastJEP: parseInt(latestJEPNo || '-1'),
+    lastDraft: lastDraftNo,
   }
 })
